df32f443ed65e08b8c7739dab3b1b5b25d1f6f08
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2024-05-30 00:42:28 -0300

    Update authController.js

--- backEnd-financial-control/app/controllers/authController.js
+++ backEnd-financial-control/app/controllers/authController.js
@@ -17,3 +17,17 @@ exports.register = async (req, res) => {
         res.status(500).json({error: error.message});
     }
 };
+
+exports.login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({where: {email}});
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(404).json({message: 'Invalid email or password'});
+        }
+        const token = jwt.singn({userId: user.id}, 'secretkey', {expiresIn: '1h'});
+        res.status(200).json({token});
+    }catch (error) {
+        res.status(500).json({error: error.message});
+    }
+};
