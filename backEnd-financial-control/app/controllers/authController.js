6eb11e9040f7372419178bc66dc1ad0ecfe51fc3
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2024-05-30 00:37:24 -0300

    Update authController.js

---
+++ backEnd-financial-control/app/controllers/authController.js
@@ -0,0 +1,19 @@
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+        res.status(201).json(user);
+        
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+};
