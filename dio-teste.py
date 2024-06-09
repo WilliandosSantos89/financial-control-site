dd8ff730f2641bb38b3aedc5a841b72e3d56c994
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2024-06-09 00:38:12 -0300

    Update dio-teste.py

--- dio-teste.py
+++ dio-teste.py
@@ -29,40 +29,32 @@ def recomendar_modelo(caracteristicas, modelos):
     return modelo_recomendado if modelo_recomendado else "Nenhum modelo encontrado."
         
 
-    modelo_recomendado = None
-  # Aqui é convertido as capacidades inseridas pelo usuário para minúsculas:
-    capacidades_usuario = [capacidade.lower() for capacidade in caracteristicas['Capacidades']]
-
-    for modelo in modelos:
-        # Convertemos as capacidades do modelo para minúsculas:
-        capacidades_modelo = [capacidade.lower() for capacidade in modelo.capacidades]
-        
-        if all(capacidade in capacidades_usuario for capacidade in capacidades_modelo):
-            if modelo_recomendado is None or modelo.desempenho > modelo_recomendado.desempenho:
-                modelo_recomendado = modelo
-
-    return modelo_recomendado if modelo_recomendado else "Nenhum modelo encontrado."
-
-# Aqui temos a função que gera uma explicação para o modelo recomendado:
 def gerar_explicacao(modelo, caracteristicas):
     if isinstance(modelo, ModeloIA):
-        explicacao = f"O {modelo.nome} é o modelo recomendado."
+        explicacao = f"O {modelo.nome} é o modelo recomendado"
         return explicacao
     else:
         return modelo
-
-# Aqui fica a função que solicita características desejadas ao usuário:
+    
 def obter_caracteristicas():
     caracteristicas = {}
-    caracteristicas['Desempenho'] = int(input())
-    caracteristicas['Velocidade'] = int(input())
-    caracteristicas['Custo'] = int(input())
-    capacidades = input().split(',')
+    caracteristicas['Desempenho'] = int(sys.stdin.readiline().strip())
+    caracteristicas['Velocidade'] = int(sys.stdin.readiline().strip())
+    caracteristicas['Custo'] = int(sys.stdin.readiline().strip())
+    capacidades = sys.stdin.readline().strip().split(',')
     caracteristicas['Capacidades'] = [capacidade.strip() for capacidade in capacidades]
     return caracteristicas
 
+#Lista de modelos Claude 3 disponíveis na plataforma Amazon Bedrock
+
+modelos = [
+    ModeloIA("Claude 3 Sonnet", 8, 10, 6, ["pesquisa", "desenvolvimento acelerado", "codificação", "recuperação de informações"]),
+    ModeloIA("Claude 3 Haiku", 7, 9, 5, ["velocidade", "resumo de dados não estruturados"]),
+    ModeloIA("Claude 3 Opus", 9, 10, 5, ["pesquisa", "desenvolvimento acelerado"]),
+]
+
 caracteristicas_entrada = obter_caracteristicas()
-melhor_modelo = recomendar_modelo(caracteristicas_entrada)
+melhor_modelo = recomendar_modelo(caracteristicas_entrada, modelos)
 explicacao = gerar_explicacao(melhor_modelo, caracteristicas_entrada)
 
 print(explicacao)
